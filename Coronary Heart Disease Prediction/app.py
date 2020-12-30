0f2703606929f0ada1c2ca0bf08163f35e0d5c88
INDRASHIS PAUL <64627762+IndraP24@users.noreply.github.com>
2020-12-30 22:28:50 +0530

    Uploaded app.py

---
+++ Coronary Heart Disease Prediction/app.py
@@ -0,0 +1,14 @@
+import utils
+# Data Loading and Numerical Operations
+import pandas as pd
+import numpy as np
+# Data Visualizations
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+data = pd.read_csv("Dataset/framingham.csv")
+
+data = utils.preprocess(data)
+
+print(data.shape, data.head())
