23ebdc55de2645be24c71c2b07db4ff85597c6dd
INDRASHIS PAUL <64627762+IndraP24@users.noreply.github.com>
2020-12-31 23:55:04 +0530

    Updated logistic regression

--- Coronary Heart Disease Prediction/model.py
+++ Coronary Heart Disease Prediction/model.py
@@ -9,7 +9,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.neural_network import MLPClassifier
-from sklearn.metrics import confusion_matrix, accuracy_score, roc_curve, classification_report
+from sklearn.metrics import accuracy_score, precision_score, recall_score
 # Hyper-parameter Tuning
 from sklearn.model_selection import RandomizedSearchCV
 from sklearn.model_selection import GridSearchCV
@@ -17,5 +17,13 @@ from sklearn.model_selection import GridSearchCV
 from mlxtend.classifier import StackingCVClassifier
 
 
+def logistic(X_train, X_test, y_train, y_test, C, max_iter):
+    lr = LogisticRegression(C=C, max_iter=max_iter)
+    lr.fit(X_train, y_train)
+    y_pred = lr.predict(X_test)
+    accuracy = lr.score(X_test, y_test)
+    return y_pred, accuracy
 
 
+def KNN(X_train, X_test, y_train, y_test, n):
+    knn = KNeighborsClassifier()
