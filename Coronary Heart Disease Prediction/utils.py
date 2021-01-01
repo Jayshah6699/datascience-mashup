e19d3fd8ebc73d48084b1e540549f14c182145a6
INDRASHIS PAUL <64627762+IndraP24@users.noreply.github.com>
2021-01-01 19:39:26 +0530

    Updated data loading persistence

--- Coronary Heart Disease Prediction/utils.py
+++ Coronary Heart Disease Prediction/utils.py
@@ -10,53 +10,65 @@ from sklearn.feature_selection import chi2
 from sklearn.model_selection import train_test_split
 # Data Scaling
 from sklearn.preprocessing import MinMaxScaler
+# Metrics
+from sklearn.metrics import plot_confusion_matrix, plot_roc_curve, plot_precision_recall_curve
+# Web App
+import streamlit as st
 
 
+@st.cache(persist=True)
+def load_data():
+    data = pd.read_csv('data/mushrooms.csv')
+    return data
+
+
+@st.cache(persist=True)
 def preprocess(data):
-	# Fill the missing spaces of glucose column with the mode of the data (Mode = 75)
-	data["glucose"].fillna((data["glucose"].mode())[0], inplace=True)
+    # Fill the missing spaces of glucose column with the mode of the data (Mode = 75)
+    data["glucose"].fillna((data["glucose"].mode())[0], inplace=True)
 
-	# Drop all other null values
-	data.dropna(inplace=True)
+    # Drop all other null values
+    data.dropna(inplace=True)
 
-	# Remove outliers
-	data = data[data['totChol']<600.0]
-	data = data[data['sysBP']<295.0]
+    # Remove outliers
+    data = data[data['totChol']<600.0]
+    data = data[data['sysBP']<295.0]
 
-	# Resample data to reduce imbalance
-	target1 = data[data['TenYearCHD'] == 1]
-	target0 = data[data['TenYearCHD'] == 0]
-	target1 = resample(target1, replace=True, n_samples=len(target0), random_state=40)
-	data = pd.concat([target0, target1])
+    # Resample data to reduce imbalance
+    target1 = data[data['TenYearCHD'] == 1]
+    target0 = data[data['TenYearCHD'] == 0]
+    target1 = resample(target1, replace=True, n_samples=len(target0), random_state=40)
+    data = pd.concat([target0, target1])
 
-	return data
+    return data
 
 
 def feature_selection(data):
-	X = data.iloc[:, 0:15]
-	y = data.iloc[:, -1]
+    X = data.iloc[:, 0:15]
+    y = data.iloc[:, -1]
 
-	best_features = SelectKBest(score_func=chi2, k=10).fit(X, y)
+    best_features = SelectKBest(score_func=chi2, k=10).fit(X, y)
 
-	data_scores = pd.DataFrame(best_features.scores_)
-	data_columns = pd.DataFrame(X.columns)
+    data_scores = pd.DataFrame(best_features.scores_)
+    data_columns = pd.DataFrame(X.columns)
 
-	scores = pd.concat([data_columns, data_scores], axis=1)
-	scores.columns = ['Feature', 'Score']
+    scores = pd.concat([data_columns, data_scores], axis=1)
+    scores.columns = ['Feature', 'Score']
 
-	# Select 10 features
-	features = scores["Feature"].tolist()[:10]
+    # Select 10 features
+    features = scores["Feature"].tolist()[:10]
 
-	return data[features]
+    return data[features]
 
 
+@st.cache(persist=True)
 def split_and_scale(data):
-	y = data['TenYearCHD']
-	X = data.drop(['TenYearCHD'], axis=1)
-	train_x, test_x, train_y, test_y = train_test_split(X, y, test_size=0.4, random_state=1)
+    y = data['TenYearCHD']
+    X = data.drop(['TenYearCHD'], axis=1)
+    train_x, test_x, train_y, test_y = train_test_split(X, y, test_size=0.4, random_state=1)
 
-	scaler = MinMaxScaler()
-	train_x = scaler.fit_transform(train_x)
-	test_x = scaler.transform(test_x)
-	
-	return train_x, test_x, train_y, test_y
+    scaler = MinMaxScaler()
+    train_x = scaler.fit_transform(train_x)
+    test_x = scaler.transform(test_x)
+
+    return train_x, test_x, train_y, test_y
